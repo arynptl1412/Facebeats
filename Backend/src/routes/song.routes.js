7cf866ab330513507122c8122329a4ef8e34a74a
Aryan Patel <arynptl1412@gmail.com>
2026-08-22 15:07:16 +0530

    Initial Commit

--- Backend/src/routes/song.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {upload} from '../middlewares/upload.middlewares.js'
-import { uploadSong } from '../controllers/song.controllers.js';
-
-const songRouter = express.Router();
-
-songRouter.post("/", upload.single("song"), uploadSong)
-
-export default songRouter;
